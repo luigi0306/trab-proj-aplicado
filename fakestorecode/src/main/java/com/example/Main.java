bd0f11c8aad489c515319bfcd57908482fd468d9
Luca Matos <lucamatosparente@gmail.com>
2025-05-07 14:24:33 -0300

    comit 6 teste

--- fakestorecode/src/main/java/com/example/Main.java
+++ fakestorecode/src/main/java/com/example/Main.java
@@ -14,6 +14,7 @@ import static spark.Spark.staticFiles;
 //comite 4
 //comite 5
 //TESTE FINAAAL
+//comit 6
 public class Main {
     public static void main(String[] args) {
         // Serve arquivos estáticos da pasta resources/public/
